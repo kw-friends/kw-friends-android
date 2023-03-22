9adbf38ed8886cb6c0bf5b83e7e74dc5b5034b20
minmul07 <sksmstmdrud@gmail.com>
2023-03-22 16:16:27 +0900

    텍스트 수정

--- app/src/main/java/hello/kwfriends/MainActivity.kt
+++ app/src/main/java/hello/kwfriends/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Bye $name!")
 }
 
 @Preview(showBackground = true)
