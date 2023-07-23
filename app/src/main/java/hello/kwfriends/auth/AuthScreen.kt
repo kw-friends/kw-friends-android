5e36576922f2b1fe18cd9dd70b8b3650e1ecbded
Seungman Lim <woori4158@gmail.com>
2023-07-23 15:28:47 +0900

    비밀번호 안전성 검사 기능 추가

--- app/src/main/java/hello/kwfriends/auth/AuthScreen.kt
+++ app/src/main/java/hello/kwfriends/auth/AuthScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
--- app/src/main/java/hello/kwfriends/auth/AuthViewModel.kt
+++ app/src/main/java/hello/kwfriends/auth/AuthViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -35,19 +36,22 @@ class AuthViewModel: ViewModel(){
         uiState = AuthUiState.Register
     }
 
+
     fun tryRegister(){
-        if(inputEmail == null || inputPassword == null){
+        if(inputEmail == null || inputPassword == null){ //이메일, 비밀번호 null 체크
             Log.w("Lim", "이메일 또는 비밀번호가 입력되지 않았습니다.")
+            return
         }
-        else if(inputPassword?.length ?: 0 <= 6){
-            Log.w("Lim", "비밀번호는 7자리 이상이여야 합니다.")
+        if (passwordSafetyCheck(inputPassword!!) == false) {
+            Log.w("Lim", "비밀번호 안전성 검사 불통과")
+            return
         }
-        else {
-            uiState = AuthUiState.Loading
-            Firebase.auth.createUserWithEmailAndPassword(inputEmail ?: "", inputPassword ?: "")
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) { //이메일 등록 성공
-                        Log.w("Lim", "이메일 등록에 성공했습니다. 인증 메일을 전송합니다.")
+        Log.w("Lim", "비밀번호 안전성 검사 통과")
+        uiState = AuthUiState.Loading
+        Firebase.auth.createUserWithEmailAndPassword(inputEmail ?: "", inputPassword ?: "")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) { //이메일 등록 성공
+                    Log.w("Lim", "이메일 등록에 성공했습니다. 인증 메일을 전송합니다.")
                         tryEmailVerify()
                         uiState = AuthUiState.Menu
                     } else {
@@ -55,8 +59,62 @@ class AuthViewModel: ViewModel(){
                         Log.w("Lim", "이메일 등록에 실패했습니다.")
                     }
                 }
-        }
     }
+
+    /*
+        [ 비밀번호 규칙 참고자료 ]
+        자바스크립트를 사용하여 비밀번호 기반 계정으로 Firebase에 인증하기 - https://firebase.google.com/docs/auth/web/password-auth?hl=ko
+        IBM Security Identity Manager(비밀번호 보안 수준 규칙) - https://www.ibm.com/docs/ko/sim/7.0.1.13?topic=rules-password-strength
+        한국보건산업진흥원 비밀번호 생성규칙 안내 - https://www.khidi.or.kr/includes/password.jsp
+        한국인터넷진흥원 패스워드 선택 및 이용 안내서 - https://www.kisa.or.kr/2060305/form?postSeq=14&lang_type=KO#fnPostAttachDownload
+        microsoft Create and use strong passwords - https://support.microsoft.com/en-us/windows/create-and-use-strong-passwords-c5cebb49-8c53-4f5e-2bc4-fe357ca048eb
+        Password Strength Checker - https://www.security.org/how-secure-is-my-password/
+         */
+    fun passwordSafetyCheck(password: String): Boolean{
+        if(inputPassword?.length ?: 0 < 8){ // 최대길이
+            Log.w("Lim", "비밀번호는 8자리 이상이여야 합니다.")
+            return false
+        }
+        else if(inputPassword?.length ?: 0 > 16){ // 최대길이
+            Log.w("Lim", "비밀번호는 16자리 이하여야 합니다.")
+            return false
+        }
+        else {
+            val special_char_list =  listOf( 33, 34, 35, 36, 37, 38, 39, 42, 58, 59, 63, 64, 92, 94, 126 ) //사용 가능한 특수문자 리스트
+            val include_special_char = mutableListOf<Char>() // 특수문자 카운트 변수
+            var include_number = mutableListOf<Char>() // 숫자 카운트 변수
+            var include_char = mutableListOf<Char>() // 문자 카운트 변수
+            for(i in inputPassword!!){
+                if(i.code in 48..57){ // 0~9까지의 숫자인지 확인
+                    include_number.add(i)
+                }
+                else if(i.code in 65..90 || i.code in 97..122){ // 대문자 또는 소문자 영어인지 확인
+                    include_char.add(i)
+                }
+                else if(i.code in special_char_list){ // 사용가능한 특수문자인지 확인
+                    include_special_char.add(i)
+                }
+                else {
+                    Log.w("Lim", "사용 불가능한 특수문자 ${i.toChar()}가 사용되었습니다.")
+                    return false
+                }
+            }
+            if(include_char.size == 0){
+                Log.w("Lim", "문자가 포함되어야 합니다.")
+                return false
+            }
+            else if(include_number.size == 0){
+                Log.w("Lim", "숫자가 포함되어야 합니다.")
+                return false
+            }
+            else if(include_special_char.size == 0){
+                Log.w("Lim", "특수문자가 포함되어야 합니다.")
+                return false
+            }
+        }
+        return true
+    }
+
     fun trySignIn(){
         Firebase.auth.signInWithEmailAndPassword(inputEmail ?: "", inputPassword ?: "")
             .addOnCompleteListener { task ->
@@ -129,5 +187,7 @@ class AuthViewModel: ViewModel(){
         Log.w("Lim", "계정을 삭제했습니다.")
         logout()
     }
+
+
 
 }
