3a036f87fd4552c12c4bd1ee1d0374e419237b5e
Minmul <68465075+minmul07@users.noreply.github.com>
2023-08-08 00:45:05 +0900

    모임 검색, 설정, 마이페이기 간 이동 구조화 누락 (MainViewModel)

---
+++ app/src/main/java/hello/kwfriends/screens/MainViewModel.kt
@@ -0,0 +1,34 @@
+package hello.kwfriends.screens
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    var uiState by mutableStateOf<MainUiState>(MainUiState.Home)
+
+    fun onClickedMyPage() {
+        viewModelScope.launch {
+            uiState = MainUiState.MyPage
+            Log.d("minmul", "onClickedMyPage")
+        }
+    }
+
+    fun onClickedSettings() {
+        viewModelScope.launch {
+            uiState = MainUiState.Settings
+            Log.d("minmul", "onClickedSettings")
+        }
+    }
+
+    fun onClickedHome() {
+        viewModelScope.launch {
+            uiState = MainUiState.Home
+            Log.d("minmul", "onClickedHome")
+        }
+    }
+}
