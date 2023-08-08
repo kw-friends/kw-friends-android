e8224e6b9e498de27367485f151121a2424de477
Minmul <68465075+minmul07@users.noreply.github.com>
2023-08-08 18:04:33 +0900

    AppBar, 설정화면 구조화

--- app/src/main/java/hello/kwfriends/screens/MainScreen.kt
+++ app/src/main/java/hello/kwfriends/screens/MainScreen.kt
@@ -6,10 +6,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import hello.kwfriends.screens.home.GatheringCard
+import hello.kwfriends.screens.settings.SettingsScreen
+
+@Composable
+fun ToolBarWithTitle(title: String, modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = { Text(text = title, fontSize = 22.sp) },
+        backgroundColor = Color(0xFFE4C5C5),
+        modifier = modifier
+    )
+}
 
 @Composable
 fun MainScreen(
@@ -17,26 +30,41 @@ fun MainScreen(
 ) {
     when (viewModel.uiState) {
         MainUiState.Home -> {
-            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxHeight()) {
-                Log.d("minmul", "Home")
-                GatheringCard()
-                BottomNavigationBar(viewModel = viewModel)
+            Column {
+                ToolBarWithTitle(title = "모임 찾기")
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    GatheringCard()
+                    BottomNavigationBar(viewModel = viewModel)
+                }
             }
         }
 
         MainUiState.Settings -> {
-            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxHeight()) {
-                Log.d("minmul", "Settings")
-                Text(text = "설정설정설정", modifier = Modifier.padding(40.dp))
-                BottomNavigationBar(viewModel = viewModel)
+            Column {
+                ToolBarWithTitle(title = "설정", modifier = Modifier.padding(bottom = 30.dp))
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    SettingsScreen()
+                    BottomNavigationBar(viewModel = viewModel)
+                }
             }
         }
 
         MainUiState.MyPage -> {
-            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxHeight()) {
-                Log.d("minmul", "Mypage")
-                Text(text = "마ㅡ이페이지마ㅡ이페이지마ㅡ이페이지마ㅡ이페이지", modifier = Modifier.padding(40.dp))
-                BottomNavigationBar(viewModel = viewModel)
+            Column {
+                ToolBarWithTitle(title = "마이페이지")
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    Text(text = "마ㅡ이페이지마ㅡ이페이지마ㅡ이페이지마ㅡ이페이지", modifier = Modifier.padding(40.dp))
+                    BottomNavigationBar(viewModel = viewModel)
+                }
             }
         }
     }
--- app/src/main/java/hello/kwfriends/screens/settings/SettingsScreen.kt
+++ app/src/main/java/hello/kwfriends/screens/settings/SettingsScreen.kt
@@ -1,16 +1,63 @@
 package hello.kwfriends.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@Preview(showSystemUi = true)
+@Composable
+fun SettingsItem(
+    title: String,
+    checked: Boolean,
+    description: String = "",
+    firstItem: Boolean = false
+) {
+    if (!firstItem) {
+        Divider(
+            color = Color.DarkGray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 10.dp)
+        )
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp)
+    ) {
+        Column(Modifier.weight(10F)) {
+            Text(text = title, fontSize = 20.sp, fontWeight = FontWeight(650))
+            if (description != "") {
+                Text(text = description, fontSize = 15.sp)
+            }
+        }
+        Switch(checked = checked, onCheckedChange = {/*TODO*/ }, Modifier.weight(2F))
+    }
+}
+
 @Composable
 fun SettingsScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Settings Screen")
+    Column {
+        SettingsItem(title = "다크 모드", checked = true, firstItem = true)
+        SettingsItem(title = "조용 모드", checked = false, description = "모든 알림을 꺼 다른 일에 집중할 수 있어요")
+        SettingsItem(
+            title = "라면에 식초 한숟갈?",
+            checked = true,
+            description = "어승경만 아는 라면 레시피, 절대 실패할 일 없어요. 진짜에요!"
+        )
     }
 }
