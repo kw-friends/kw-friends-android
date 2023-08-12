ec5f6e31e6110793e81c0dd415b183e134cc3532
Minmul <68465075+minmul07@users.noreply.github.com>
2023-08-12 20:26:36 +0900

    material3 변경, 앱 테마 구성

--- app/src/main/java/hello/kwfriends/auth/AuthMainActivity.kt
+++ app/src/main/java/hello/kwfriends/auth/AuthMainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import hello.kwfriends.auth.ui.theme.KWFriendsTheme
+import hello.kwfriends.ui.theme.KWFriendsTheme
 
 class AuthMainActivity : ComponentActivity() {
 
