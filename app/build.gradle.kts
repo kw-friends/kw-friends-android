a12c552d395a3a57149ffd2ecef816bb06e29768
Minmul <68465075+minmul07@users.noreply.github.com>
2023-10-29 23:11:07 +0900

    AGP Update / dependencies Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,29 +45,29 @@ android {
     }
 }
 
-val composeUiVersion = "1.5.2"
-val navVersion = "2.7.3"
+val composeUiVersion = "1.5.4"
+val navVersion = "2.7.4"
 
 dependencies {// mode
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui:$composeUiVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
-    implementation("androidx.compose.material:material:1.5.2")
+    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.compose.ui:ui-graphics")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeUiVersion")
-    androidTestImplementation("androidx.compose:compose-bom:2023.09.02")
-    implementation("androidx.compose:compose-bom:2023.09.02")
-    implementation("com.google.firebase:firebase-bom:32.3.1")
+    androidTestImplementation("androidx.compose:compose-bom:2023.10.01")
+    implementation("androidx.compose:compose-bom:2023.10.01")
+    implementation("com.google.firebase:firebase-bom:32.4.1")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -78,7 +78,7 @@ dependencies {// mode
     implementation("androidx.compose.material:material-icons-extended:$composeUiVersion")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.4.1"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("androidx.datastore:datastore-preferences:1.0.0") //preference datastore
 }
