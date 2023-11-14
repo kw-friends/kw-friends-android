57bebb7fc807958731b0cceeae7eb815c3e8ea9f
Seungman Lim <woori4158@gmail.com>
2023-11-14 12:04:34 +0900

    이미지 선택 및 출력 방법 수정

--- app/src/main/java/hello/kwfriends/ui/screens/profileImageScreen/ProfileImageScreen.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/profileImageScreen/ProfileImageScreen.kt
@@ -1,18 +1,15 @@
 package hello.kwfriends.ui.screens.profileImageScreen
 
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -31,25 +28,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import hello.kwfriends.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileImageScreen(navigation: NavController) {
-    var imageUri by remember {
-        mutableStateOf<Uri?>(null)
-    }
-    val context = LocalContext.current
-    val bitmap =  remember {
-        mutableStateOf<Bitmap?>(null)
-    }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
 
     val launcher = rememberLauncherForActivityResult(contract =
-    ActivityResultContracts.GetContent()) { uri: Uri? ->
+    ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
         imageUri = uri
     }
 
@@ -80,40 +73,34 @@ fun ProfileImageScreen(navigation: NavController) {
                 }
             )
         }
-    ) {
+    ) { paddingValues ->
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .padding(it)
+                .padding(paddingValues)
                 .fillMaxSize()
         ) {
-            imageUri?.let {
-                if (Build.VERSION.SDK_INT < 28) {
-                    bitmap.value = MediaStore.Images
-                        .Media.getBitmap(context.contentResolver,it)
+            AsyncImage(
+                model = imageUri ?: R.drawable.profile_default_image, // Use a default image resource
+                contentDescription = "profile image",
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop,
+            )
 
-                } else {
-                    val source = ImageDecoder
-                        .createSource(context.contentResolver,it)
-                    bitmap.value = ImageDecoder.decodeBitmap(source)
-                }
-
-                bitmap.value?.let { btm ->
-                    Image(bitmap = btm.asImageBitmap(),
-                        contentDescription =null,
-                        modifier = Modifier.size(400.dp))
-                }
-            }
-            //Image(painter = painterResource(id = R.drawable.profile_default_image), contentDescription = "profile_image")
             Button(
                 modifier = Modifier.padding(top = 25.dp),
-                onClick = { launcher.launch("image/*") }
+                onClick = {
+                    launcher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }
             ) {
                 Text("이미지 선택")
             }
         }
     }
 }
-
-//참고 - https://ngengesenior.medium.com/pick-image-from-gallery-in-jetpack-compose-5fa0d0a8ddaf
