f67575e72fe6f6dd0336f8070b8402a3423281fa
Seungman Lim <woori4158@gmail.com>
2023-11-24 17:55:37 +0900

    검색 결과 없음 표시

--- app/src/main/java/hello/kwfriends/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/home/HomeScreen.kt
@@ -3,7 +3,10 @@
 package hello.kwfriends.ui.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
@@ -14,6 +17,7 @@ import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import hello.kwfriends.ui.component.HomeTopAppBar
@@ -54,11 +59,13 @@ fun MainScreen(
             homeViewModel.refreshPost()
         }
     )
+    //검색창에 대한 포커스
     val focusRequester = remember { FocusRequester() }
     LaunchedEffect(!homeViewModel.isSearching) {
         focusRequester.requestFocus()
     }
     Scaffold(
+        //앱 바
         topBar = {
             HomeTopAppBar(
                 navigation =  navigation,
@@ -70,6 +77,7 @@ fun MainScreen(
                 focusRequester = focusRequester,
             )
         },
+        //플로팅 버튼
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 text = { Text(text = "모임 생성") },
@@ -86,12 +94,39 @@ fun MainScreen(
                 .padding(it)
                 .pullRefresh(pullRefreshState)
         ) {
+            //검색중인지
             if(homeViewModel.isSearching) {
-                FindGatheringCardList(homeViewModel.searchingPosts, viewModel = homeViewModel)
+                if(homeViewModel.searchContent != "" && homeViewModel.searchingPosts.isEmpty()) {
+                    //검색 결과 없을때 표시할 화면
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Box(modifier = Modifier.weight(1f))
+                        Column(
+                            modifier = Modifier.weight(6f),
+                            verticalArrangement = Arrangement.Top
+                        ) {
+                            Text(
+                                text = " \"${homeViewModel.searchContent}\"에 대한 검색 결과가 없습니다",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = Color.DarkGray,
+                                modifier = Modifier.padding(horizontal = 20.dp),
+                                maxLines = 100
+                            )
+                        }
+                    }
+                }
+                else {
+                    //검색 결과 화면
+                    FindGatheringCardList(homeViewModel.searchingPosts, viewModel = homeViewModel)
+                }
             }
+            //검색중 아닐때는 모든 모임 목록 표시
             else {
                 FindGatheringCardList(homeViewModel.posts, viewModel = homeViewModel)
             }
+            //로딩 아이콘
             PullRefreshIndicator(
                 refreshing = homeViewModel.isRefreshing,
                 state = pullRefreshState,
