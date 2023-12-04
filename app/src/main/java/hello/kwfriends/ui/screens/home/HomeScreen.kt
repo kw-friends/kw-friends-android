0bc3d3d788198317fb7b8013d739dcb519e4f403
Seungman Lim <woori4158@gmail.com>
2023-12-04 14:49:00 +0900

    포스트 다이얼로그 스크롤 기능 추가
    issue-42

--- app/src/main/java/hello/kwfriends/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/home/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
@@ -139,6 +140,7 @@ fun HomeScreen(
         //포스트 정보 다이얼로그
         if (homeViewModel.postDialogState.first && homeViewModel.postDialogState.second != null) {
             var menuExpanded by remember { mutableStateOf(false) }
+            val scrollState = rememberScrollState()
             Dialog(
                 onDismissRequest = { homeViewModel.postDialogState = false to null },
                 properties = DialogProperties(
@@ -146,6 +148,7 @@ fun HomeScreen(
                 )
             ) {
                 Box(modifier = Modifier
+                    .verticalScroll(scrollState)
                     .fillMaxSize()
                     .background(Color.White)
                 ) {
