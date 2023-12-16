dff6daf0178a9942153bb68b55a14678af0bd8e5
Seungman Lim <woori4158@gmail.com>
2023-12-16 15:59:05 +0900

    newPostScreen 들어갈때마다 입력값 및 변수 초기화
    newPostScreen 나갔다들어오거나 포스트 생성한 후 다시 들어가면 입력값 남아있는 오류 해결
    
    issue-42

--- app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostScreen.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -69,6 +70,9 @@ fun NewPostScreen(
             actionOnNewLine = true,
             snackbarData = data
         )
+    }
+    LaunchedEffect(true) {
+        newPostViewModel.initInput()
     }
     Box(
         modifier = Modifier
--- app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostViewModel.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostViewModel.kt
@@ -7,10 +7,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import hello.kwfriends.Tags.Tags
 import hello.kwfriends.firebase.firestoreDatabase.PostManager
-import hello.kwfriends.ui.main.Routes
 import hello.kwfriends.ui.screens.auth.AuthViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,7 +18,7 @@ class NewPostViewModel : ViewModel() {
     var gatheringTitle by mutableStateOf("")
     var gatheringTitleStatus by mutableStateOf(false)
 
-    var gatheringPromoter by mutableStateOf(AuthViewModel.userInfo!!["name"].toString())
+    var gatheringPromoter by mutableStateOf("")
 
     var gatheringTime by mutableStateOf("")
 
@@ -38,11 +36,7 @@ class NewPostViewModel : ViewModel() {
     val snackbarEvent: StateFlow<String?> get() = _snackbarEvent
 
     //태그 저장 변수
-    var tagMap = mutableStateMapOf<String, Boolean>().apply {
-        Tags.list.forEach { tag ->
-            this[tag] = false
-        }
-    }
+    var tagMap = mutableStateMapOf<String, Boolean>()
 
     fun showSnackbar(message: String) {
         _snackbarEvent.value = message
@@ -91,6 +85,22 @@ class NewPostViewModel : ViewModel() {
         uploadResult = result
     }
 
+    fun initInput() {
+        gatheringPromoter = AuthViewModel.userInfo!!["name"].toString()
+        gatheringTitle = ""
+        gatheringTitleStatus = false
+        gatheringTime = ""
+        gatheringLocation = ""
+        gatheringDescription = ""
+        gatheringDescriptionStatus = false
+        maximumParticipants = ""
+        participantsRangeValidation = false
+        tagMap = mutableStateMapOf<String, Boolean>().apply {
+            Tags.list.forEach { tag ->
+                this[tag] = false
+            }
+        }
+    }
 
     fun uploadGatheringToFirestore(end: () -> Unit) {
         showSnackbar("모임 생성 중...")
