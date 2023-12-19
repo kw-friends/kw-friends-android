dedfa96fb83af8e076410b635e380d527e22324b
Seungman Lim <woori4158@gmail.com>
2023-12-19 16:45:40 +0900

    모임 생성 화면 태그 바로 업데이트 안되는 오류 해결
    issue-049

--- app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostViewModel.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/newPost/NewPostViewModel.kt
@@ -2,7 +2,6 @@ package hello.kwfriends.ui.screens.newPost
 
 import android.util.Log
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
@@ -40,7 +39,7 @@ class NewPostViewModel : ViewModel() {
     val snackbarEvent: StateFlow<String?> get() = _snackbarEvent
 
     //태그 저장 변수
-    var tagMap = mutableStateMapOf<String, Boolean>()
+    var tagMap by mutableStateOf<MutableMap<String, Boolean>>(mutableMapOf())
 
     fun showSnackbar(message: String) {
         _snackbarEvent.value = message
@@ -85,6 +84,12 @@ class NewPostViewModel : ViewModel() {
                 participantsRangeValidation)
     }
 
+    fun updateTagMap(tag: String) {
+        tagMap = tagMap.toMutableMap().apply {
+            this[tag] = !(tagMap[tag] ?: true)
+        }
+    }
+
     fun initInput() {
         gatheringPromoter = UserData.userInfo!!["name"].toString()
         gatheringTitle = ""
@@ -95,7 +100,7 @@ class NewPostViewModel : ViewModel() {
         gatheringDescriptionStatus = false
         maximumParticipants = ""
         participantsRangeValidation = false
-        tagMap = mutableStateMapOf<String, Boolean>().apply {
+        tagMap = tagMap.toMutableMap().apply {
             Tags.list.forEach { tag ->
                 this[tag] = false
             }
