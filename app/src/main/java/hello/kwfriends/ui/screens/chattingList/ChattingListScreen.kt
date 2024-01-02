7606af1c0b2d5df83cee86a116ded67db1be71b0
Seungman Lim <woori4158@gmail.com>
2024-01-02 22:09:50 +0900

    채팅방 임시 디자인 추가
    issue-054-userInfoPopup

--- app/src/main/java/hello/kwfriends/ui/screens/chattingList/ChattingListScreen.kt
+++ app/src/main/java/hello/kwfriends/ui/screens/chattingList/ChattingListScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import hello.kwfriends.ui.main.Routes
@@ -53,10 +58,44 @@ fun ChattingListScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 60.dp, bottom = 40.dp, start = 20.dp, end = 20.dp)
         ) {
             //top
-
+            Box(
+                modifier = Modifier
+                    .padding(top = 60.dp)
+                    .fillMaxWidth()
+            ) {
+                Box(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                ) {
+                    Column(modifier = Modifier.align(Alignment.TopStart)) {
+                        Text(
+                            text = "테스트용 채팅방",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight(600)
+                        )
+                        Text(
+                            text = "새해 복 많이받으세요~!",
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontFamily = FontFamily.Default,
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.align(Alignment.TopEnd),
+                        text = "2023/12/31",
+                        style = MaterialTheme.typography.bodySmall,
+                        fontFamily = FontFamily.Default,
+                        color = Color.Gray
+                    )
+                }
+            }
+            Divider(
+                modifier = Modifier.padding(horizontal = 5.dp),
+                color = Color.LightGray,
+                thickness = 0.5.dp,
+            )
         }
     }
 }
