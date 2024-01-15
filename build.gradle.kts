0dc8f561caf5681e49e575ba3c3a79cd01da27cc
Minmul <68465075+minmul07@users.noreply.github.com>
2024-01-16 00:31:14 +0900

    Build Script 에러 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id ("com.android.library") version "8.2.1" apply false
     id ("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id ("com.google.gms.google-services") version "4.3.14" apply false
-    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
