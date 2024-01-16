0fcdd2fb72718e63e536712a03eea0d9c49d5cd1
Seungman Lim <woori4158@gmail.com>
2024-01-16 15:14:52 +0900

    Add: Chattings object class 추가
    issue-040-chatting

---
+++ app/src/main/java/hello/kwfriends/firebase/realtimeDatabase/Chattings.kt
@@ -0,0 +1,9 @@
+package hello.kwfriends.firebase.realtimeDatabase
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+object Chattings {
+    private var database = Firebase.database
+
+}
